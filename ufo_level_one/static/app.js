aded2e0796ead8f41f9ff95f61d99e385adb427c
zack mccann <zackjmccann@gmail.com>
2019-12-05 11:38:47 -0700

    filtered data

--- ufo_level_one/static/app.js
+++ ufo_level_one/static/app.js
@@ -1,2 +1,23 @@
-// from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+console.log(data);
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    d3.event.preventDefault;
+    var inputValue = d3.select('#datetime').property('value');
+    function filteredData(sighting_date) {
+        return sighting_date.datetime === inputValue;
+    };
+
+    var filteredSightings = data.filter(filteredData);
+
+    filteredSightings.forEach((filteredSightings) => {
+        var row = tbody.append("tr");
+        Object.entries(filteredSightings).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+});
